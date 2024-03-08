41c9fbb55de98bcabddd0b24a89612ec0fa2ab8e
Dustin Andrews <51100082+dbandrews@users.noreply.github.com>
2024-03-07 21:14:41 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,18 @@ ln -sfn ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sfn ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sfn ~/dotfiles/.zshrc ~/.zshrc
 ln -sfn ~/dotfiles/.aliases ~/.aliases
+ln -sfn ~/dotfiles/settings.json ~/.vscode-server/data/Machine/settings.json
+
+echo "Setting up Git"
+
+defaultName=$( git config --global user.name )
+defaultEmail=$( git config --global user.email )
+
+read -rp "Name [$defaultName] " name
+read -rp "Email [$defaultEmail] " email
+
+git config --global user.name "${name:-$defaultName}"
+git config --global user.email "${email:-$defaultEmail}"
+git config --global core.excludesfile ~/.gitignore_global
+
+conda config --set changeps1 False
